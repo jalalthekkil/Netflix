29a877f7196c1c3d5abc7d1feef14e7507712f95
jalalthekkil <jalalthekkil@gmail.com>
2023-08-31 13:01:51 +0530

    changes made

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import './App.css'
+import { action, originals } from './urls'
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action ' isSmall />
+    </div>
+  )
+}
+
+export default App
